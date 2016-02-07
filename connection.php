2d414498e782faab73a0e1957c67a09b47185bf7
Sheel Shah <shahsheel@outlook.com>
2016-02-07 19:39:24 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
     <?php
     $mysql_hostname = "localhost";
     $mysql_user = "root";
-    $mysql_password = "football10";
+    $mysql_password = "**********";
     $mysql_database = "backup";
     $prefix = "";
     $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
